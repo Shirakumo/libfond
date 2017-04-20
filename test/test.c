f56d9c93804669a91ff477faec70fb03d1ccf1e2
Shinmera <shinmera@tymoon.eu>
2017-04-20 23:25:07 +0200

    I am dumb and also tired.

--- test/test.c
+++ test/test.c
@@ -121,9 +121,9 @@ int main(int argc, char **argv){
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2*3*sizeof(float), (GLvoid *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (GLvoid *)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2*3*sizeof(float), (GLvoid *)(3*sizeof(float)));
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 6*sizeof(float), (GLvoid *)(3*sizeof(float)));
   glEnableVertexAttribArray(1);
 
   glGenBuffers(1, &ebo);
